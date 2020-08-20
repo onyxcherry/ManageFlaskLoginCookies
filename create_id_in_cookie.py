8d688b357d95329ac96fe877df9b1a5a0308ada4
Tomek <tomek@onyxcherry.pl>
2020-08-20 17:47:45 +0200

    Rename file and function

--- create_id_in_cookie.py
+++ create_id_in_cookie.py
@@ -1,6 +1,6 @@
 from hashlib import sha512
 
-def create_cookie(remote_address: str, user_agent: str) -> str:
+def create_id_in_cookie(remote_address: str, user_agent: str) -> str:
     
     base = f"{remote_address.encode('utf-8')}|{user_agent.encode('utf-8')}"
     h = sha512()
@@ -10,4 +10,4 @@ def create_cookie(remote_address: str, user_agent: str) -> str:
 remote_address = '127.0.0.1'
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
 
-print(create_cookie(remote_address, user_agent))
+print(create_id_in_cookie(remote_address, user_agent))
